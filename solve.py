a4bfa26c908856ed2e323f51da3b756e12c59ea6
Yusuf Ismail <82184077+is-yusuf@users.noreply.github.com>
2022-05-24 20:39:43 -0500

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,4 +1,4 @@
-import numba
+# import numba
 import time
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,6 +10,9 @@ import os
 from datetime import datetime
 import warnings
 warnings.filterwarnings("ignore")
+import threading
+from queue import Queue
+
 
 
 class oscillator:
@@ -30,10 +33,10 @@ class oscillator:
             f0 = p
             f1 = -β**2 * x**3 + (1-3*β**2*χ**2) * x - 2 * \
                 Γ*p + g/β * math.cos(ω*t)
-            f2 = Π + Γ*((χ-χ**3+χ*Π**2-1/(4*χ))*cos2ϕ - Π*(-1+2*χ**2)
-                        * sin2ϕ + χ - χ**3 - χ*Π**2 + 1/(4*χ))
-            f3 = χ*(1-3*β**2*(x**2 + χ**2)) + 1/(4*χ**3) + Γ*((Π**3 - Π + 3*Π/(4*χ**2) - Π*χ**2) * cos2ϕ
-                                                              - (-1/(4*χ**2)+1/χ-χ+2*χ*Π**2)*sin2ϕ
+            f2 = Π + Γ*((χ-χ**3+χ*Π**2-1/(4*χ))*math.cos(2*ϕ) - Π*(-1+2*χ**2)
+                        * math.sin(2*ϕ) + χ - χ**3 - χ*Π**2 + 1/(4*χ))
+            f3 = χ*(1-3*β**2*(x**2 + χ**2)) + 1/(4*χ**3) + Γ*((Π**3 - Π + 3*Π/(4*χ**2) - Π*χ**2) * math.cos(2*ϕ)
+                                                              - (-1/(4*χ**2)+1/χ-χ+2*χ*Π**2)*math.sin(2*ϕ)
                                                               + (-Π**3 - Π - 3*Π/(4*χ**2)-Π*χ**2))
             return np.array([f0, f1, f2, f3])
 
@@ -43,10 +46,16 @@ class oscillator:
             χ = y[2]
             Π = y[3]
             return np.diag([
-                sqrtΓ*(2*(χ ** 2 - 1/2)*cosϕ + 2 * χ*Π*sinϕ),
-                sqrtΓ * (2 * (1/(4*χ**2) + Π**2 - 1/2)*sinϕ+2*χ*Π*cosϕ),
+                sqrtΓ*(2*(χ ** 2 - 1/2)*math.cos(ϕ) + 2 * χ*Π*math.sin(ϕ)),
+                sqrtΓ * (2 * (1/(4*χ**2) + Π**2 - 1/2)*math.sin(ϕ)+2*χ*Π*math.cos(ϕ)),
                 0,
                 0])
+            # return np.diag([
+            #     sqrtΓ*(2*(χ ** 2 - 1/2)*cosϕ + 2 * χ*Π*sinϕ),
+            #     sqrtΓ * (2 * (1/(4*χ**2) + Π**2 - 1/2)*sinϕ+2*χ*Π*cosϕ),
+            #     0,
+            #     0])
+
         start_time = time.time()
         T = 2*np.pi/ω
         xs = sdeint.itoint(f, W, y0, t)
@@ -62,7 +71,6 @@ class oscillator:
         plt.savefig(os.path.join(
             folder, "acc={} gamma={}, beta={}.png".format(acc, Γ, β)))
 
-    @numba.njit(target='cuda')
     def bifurcation(self, arr):
         β, g, ω, Γ, ϕ, y0, t, periods, acc, folder = arr
         cosϕ = math.cos(ϕ)
@@ -70,7 +78,6 @@ class oscillator:
         sinϕ = math.sin(ϕ)
         sin2ϕ = math.cos(2*ϕ)
         sqrtΓ = math.sqrt(Γ)
-
         def f(y, t):
             x = y[0]
             p = y[1]
@@ -80,14 +87,41 @@ class oscillator:
             f0 = p
             f1 = -β**2 * x**3 + (1-3*β**2*χ**2) * x - 2 * \
                 Γ*p + g/β * math.cos(ω*t)
-            f2 = Π + Γ*((χ-χ**3+χ*Π**2-1/(4*χ))*cos2ϕ - Π*(-1+2*χ**2)
-                        * sin2ϕ + χ - χ**3 - χ*Π**2 + 1/(4*χ))
-            f3 = χ*(1-3*β**2*(x**2 + χ**2)) + 1/(4*χ**3) + Γ*((Π**3 - Π + 3*Π/(4*χ**2) - Π*χ**2) * cos2ϕ
-                                                              - (-1/(4*χ**2)+1/χ-χ+2*χ*Π**2)*sin2ϕ
+            f2 = Π + Γ*((χ-χ**3+χ*Π**2-1/(4*χ))*math.cos(2*ϕ) - Π*(-1+2*χ**2)
+                        * math.sin(2*ϕ) + χ - χ**3 - χ*Π**2 + 1/(4*χ))
+            f3 = χ*(1-3*β**2*(x**2 + χ**2)) + 1/(4*χ**3) + Γ*((Π**3 - Π + 3*Π/(4*χ**2) - Π*χ**2) * math.cos(2*ϕ)
+                                                              - (-1/(4*χ**2)+1/χ-χ+2*χ*Π**2)*math.sin(2*ϕ)
                                                               + (-Π**3 - Π - 3*Π/(4*χ**2)-Π*χ**2))
             return np.array([f0, f1, f2, f3])
 
         def W(y, t):
+            x = y[0]
+            p = y[1]
+            χ = y[2]
+            Π = y[3]
+            return np.diag([
+                sqrtΓ*(2*(χ ** 2 - 1/2)*math.cos(ϕ) + 2 * χ*Π*math.sin(ϕ)),
+                sqrtΓ * (2 * (1/(4*χ**2) + Π**2 - 1/2)*math.sin(ϕ)+2*χ*Π*math.cos(ϕ)),
+                0,
+                0])
+        
+        # def f(y, t):
+        #     x = y[0]
+        #     p = y[1]
+        #     χ = y[2]
+        #     Π = y[3]
+
+        #     f0 = p
+        #     f1 = -β**2 * x**3 + (1-3*β**2*χ**2) * x - 2 * \
+        #         Γ*p + g/β * math.cos(ω*t)
+        #     f2 = Π + Γ*((χ-χ**3+χ*Π**2-1/(4*χ))*cos2ϕ - Π*(-1+2*χ**2)
+        #                 * sin2ϕ + χ - χ**3 - χ*Π**2 + 1/(4*χ))
+        #     f3 = χ*(1-3*β**2*(x**2 + χ**2)) + 1/(4*χ**3) + Γ*((Π**3 - Π + 3*Π/(4*χ**2) - Π*χ**2) * cos2ϕ
+        #                                                       - (-1/(4*χ**2)+1/χ-χ+2*χ*Π**2)*sin2ϕ
+        #                                                       + (-Π**3 - Π - 3*Π/(4*χ**2)-Π*χ**2))
+        #     return np.array([f0, f1, f2, f3])
+
+        # def W(y, t):
             x = y[0]
             p = y[1]
             χ = y[2]
@@ -105,19 +139,16 @@ class oscillator:
 
         distances = [math.sqrt((β*x[i])**2+(β*y[i])**2)
                      for i in range(len(x))]
-        return [β, distances]
+        # return [β, distances]
+        return [ϕ, distances[int (len(distances)/10):]]
 
     def bifurcateβ(self, g, ω, Γ, ϕ, y0, t, periods, acc, folder, points):
-        # βs = np.linspace(0.00001, 1, 10)
         βs = np.linspace(0.1, 1, points)
-        # np.array([0.03901, 0.5, 0.7])
-
         callinglist = np.array(
-            [[βs[0], g, ω, Γ, ϕ, y0, t, periods, acc, folder]])
-
+            [[βs[0], g, ω, Γ, ϕ, y0, t, periods, acc, folder]], dtype=object)
         for i in βs:
             callinglist = np.append(
-                callinglist, np.array([[i, g, ω, Γ, ϕ, y0, t, periods, acc, folder]]), axis=0)
+                callinglist, np.array([[i, g, ω, Γ, ϕ, y0, t, periods, acc, folder]], dtype=object), axis=0)
         start_time = time.time()
         with concurrent.futures.ProcessPoolExecutor() as executor:
             results = executor.map(self.bifurcation, callinglist)
@@ -125,7 +156,7 @@ class oscillator:
 
         for result in results:
             plt.scatter([result[0]]*len(result[1]), result[1],
-                        color='blue', s=periods/(acc*10))
+                        color='blue', s=periods/(acc*100), alpha=.45)
 
         plt.xlabel('β/β', fontsize=10)
         plt.ylabel('d/β', fontsize=10)
@@ -134,6 +165,31 @@ class oscillator:
         plt.savefig(os.path.join(
             folder, "acc={} gamma={}, beta={}.png".format(acc, Γ, β)))
 
+    def bifurcateϕ(self, g, ω, β, Γ, y0, t, periods, acc, folder, points):
+        ϕs = np.linspace(0.0001, math.pi/2, points)
+        callinglist = np.array(
+            [[β, g, ω, Γ, ϕs[0], y0, t, periods, acc, folder]], dtype=object)
+        for i in ϕs:
+            callinglist = np.append(
+                callinglist, np.array([[β, g, ω, Γ, i, y0, t, periods, acc, folder]], dtype=object), axis=0)
+        start_time = time.time()
+
+        
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(self.bifurcation, callinglist)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+        for result in results:
+            plt.scatter([result[0]]*len(result[1]), result[1],
+                        color='blue', s=periods/(acc*300), alpha=.6)
+
+        plt.xlabel('β/β', fontsize=10)
+        plt.ylabel('d/β', fontsize=10)
+        plt.tick_params(labelsize=10)
+        plt.title('The bifurcation diagram')
+        plt.savefig(os.path.join(
+            folder, "phibifurcation gamma = {} and beta = {}.png".format(Γ, β)))
+
 
 if __name__ == '__main__':
     g = 0.3
@@ -141,8 +197,8 @@ if __name__ == '__main__':
     β = 0.03901
     Γ = 0.110
     ϕ = 0.5
-    periods = 400
-    acc = 100
+    periods = 500
+    acc = 150
 
     y0 = np.array([0, 0.1, 0.1, 0.001])
     T = 2*np.pi/ω
@@ -155,8 +211,9 @@ if __name__ == '__main__':
     points = 10
     os.mkdir(os.path.join(cwd, current_time))
     folder = os.path.join(cwd, current_time)
+    # osc.bifurcateβ(g, ω, Γ, ϕ, y0, t, periods, acc, folder, points)
 
-    osc.bifurcateβ(g, ω, Γ, ϕ, y0, t, periods, acc, folder, points)
+    osc.bifurcateϕ(g, ω, β, Γ, y0, t, periods, acc, folder, points)
 
     # start_time = time.time()
 
